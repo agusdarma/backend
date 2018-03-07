aa0ab793495cb228ccc2bbcd804104a0f3acc124
Agus Darma Kusuma <agus.darma@emobile.com.sg>
2018-03-07 17:48:06 +0700

    Learning

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,53 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('welcome2');
+});
+
+Route::get('/admin/{id}', function ($id) {
+        echo 'ID: '.$id;
+});
+
+Route::get('/user/{name?}',function($name = 'Virat'){
+    echo "Name: ".$name;
+});
+
+Route::get('role',[
+    'middleware' => 'role:editor',
+    'uses' => 'TestController@index',
+]);
+
+Route::get('age',[
+    'middleware' => 'Age:editor agus',
+    'uses' => 'TestController@index',
+]);
+
+Route::get('terminate',[
+    'middleware' => 'terminate',
+    'uses' => 'ABCController@index',
+]);
+
+Route::get('/usercontroller/path',[
+    'middleware' => 'first',
+    'uses' => 'UserController@showPath'
+]);
+
+Route::resource('my','MyController');  
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/user/register',array('uses'=>'UserRegistration@postRegister'));
+
+Route::get('/cookie/set','CookieController@setCookie');
+Route::get('/cookie/get','CookieController@getCookie');
+
+Route::get('blade', function () {
+    return view('page',array('name' => 'Virat Gandhi'));
+});
+
+Route::get('insert','StudInsertController@insertform');
+Route::post('create','StudInsertController@insert');
