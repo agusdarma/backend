6331c17756814b2525d0c90126edfdc34ea20721
agus <agusdk2011@gmail.com>
2018-03-11 23:08:17 +0700

    update db user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -62,9 +62,16 @@ class RegisterController extends Controller
      * @return \App\User
      */
     protected function create(array $data)
-    {
+    {  
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => 'testing',
+            'username' => 'testing',
+            'status' => 'Active',
+            'gender' => 'Male',
+            'group_id' => '1',
+            'store' => 'DKI',
+            'phone_no' => '085693938630',
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- app/User.php
+++ app/User.php
@@ -10,12 +10,13 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
+     * The attributes that are mass assignable
+
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name','last_name','phone_no','username','status'
+        ,'gender','group_id', 'email','store', 'password',
     ];
 
     /**
