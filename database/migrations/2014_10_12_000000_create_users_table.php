c220750bd9cde299acb2d850e5c05624c2ac484c
agus <agusdk2011@gmail.com>
2018-03-11 23:08:59 +0700

    update db user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name','64');
+            $table->string('first_name','64')->nullable();
+            $table->string('last_name','64')->nullable();
             $table->string('email','128')->unique();
+            $table->string('phone_no','32')->unique();
+            $table->integer('group_id')->nullable();
+            $table->string('gender','16')->nullable();
+            $table->string('username','64')->nullable();
             $table->string('password','256');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('status','32');
+            $table->string('store','32')->nullable();
+            $table->string('remember_token','100')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->engine = 'InnoDB';
         });
     }
 
