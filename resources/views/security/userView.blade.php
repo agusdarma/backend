19d1dd72e9ae1e8a7eee882355090268fd7dccfb
Agus Darma Kusuma <agus.darma@emobile.com.sg>
2018-04-13 16:28:27 +0700

    update

--- resources/views/security/userView.blade.php
+++ resources/views/security/userView.blade.php
@@ -291,11 +291,7 @@
           {
             table = $(tableId).dataTable();
             oSettings = table.fnSettings();
-            table.fnClearTable(this);
-            for (var i=0; i<json.aaData.length; i++)
-            {
-              table.oApi._fnAddData(oSettings, json.aaData[i]);
-            }
+            table.fnClearTable(this);            
             oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
             table.fnDraw();
           });
